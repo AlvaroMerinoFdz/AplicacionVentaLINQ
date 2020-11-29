440015f1b4fa2080d402396230a9829020f26bc1
Alvaro Merino <alvaro.merino.fernandez@gmail.com>
2020-11-29 14:45:20 +0100

    Diseño Realizado e Icono agregado

--- AplicacionVentaLINQ/Principal.Designer.cs
+++ AplicacionVentaLINQ/Principal.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace AplicacionVentaLINQ
 {
-    partial class Form1
+    partial class Principal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,13 +29,156 @@ namespace AplicacionVentaLINQ
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnConsulta1 = new System.Windows.Forms.Button();
+            this.btnConsulta2 = new System.Windows.Forms.Button();
+            this.btnConsulta3 = new System.Windows.Forms.Button();
+            this.btnConsulta4 = new System.Windows.Forms.Button();
+            this.btnConsulta5 = new System.Windows.Forms.Button();
+            this.btnConsulta6 = new System.Windows.Forms.Button();
+            this.btnConsulta7 = new System.Windows.Forms.Button();
+            this.btnConsulta8 = new System.Windows.Forms.Button();
+            this.btnConsulta9 = new System.Windows.Forms.Button();
+            this.btnConsulta10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 170);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(835, 221);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnConsulta1
+            // 
+            this.btnConsulta1.Location = new System.Drawing.Point(19, 27);
+            this.btnConsulta1.Name = "btnConsulta1";
+            this.btnConsulta1.Size = new System.Drawing.Size(197, 23);
+            this.btnConsulta1.TabIndex = 1;
+            this.btnConsulta1.Text = "Consulta1";
+            this.btnConsulta1.UseVisualStyleBackColor = true;
+            // 
+            // btnConsulta2
+            // 
+            this.btnConsulta2.Location = new System.Drawing.Point(19, 74);
+            this.btnConsulta2.Name = "btnConsulta2";
+            this.btnConsulta2.Size = new System.Drawing.Size(197, 23);
+            this.btnConsulta2.TabIndex = 2;
+            this.btnConsulta2.Text = "Consulta2";
+            this.btnConsulta2.UseVisualStyleBackColor = true;
+            // 
+            // btnConsulta3
+            // 
+            this.btnConsulta3.Location = new System.Drawing.Point(19, 123);
+            this.btnConsulta3.Name = "btnConsulta3";
+            this.btnConsulta3.Size = new System.Drawing.Size(197, 23);
+            this.btnConsulta3.TabIndex = 3;
+            this.btnConsulta3.Text = "Consulta3";
+            this.btnConsulta3.UseVisualStyleBackColor = true;
+            // 
+            // btnConsulta4
+            // 
+            this.btnConsulta4.Location = new System.Drawing.Point(233, 27);
+            this.btnConsulta4.Name = "btnConsulta4";
+            this.btnConsulta4.Size = new System.Drawing.Size(197, 23);
+            this.btnConsulta4.TabIndex = 4;
+            this.btnConsulta4.Text = "Consulta4";
+            this.btnConsulta4.UseVisualStyleBackColor = true;
+            // 
+            // btnConsulta5
+            // 
+            this.btnConsulta5.Location = new System.Drawing.Point(233, 74);
+            this.btnConsulta5.Name = "btnConsulta5";
+            this.btnConsulta5.Size = new System.Drawing.Size(197, 23);
+            this.btnConsulta5.TabIndex = 5;
+            this.btnConsulta5.Text = "Consulta5";
+            this.btnConsulta5.UseVisualStyleBackColor = true;
+            // 
+            // btnConsulta6
+            // 
+            this.btnConsulta6.Location = new System.Drawing.Point(233, 123);
+            this.btnConsulta6.Name = "btnConsulta6";
+            this.btnConsulta6.Size = new System.Drawing.Size(197, 23);
+            this.btnConsulta6.TabIndex = 6;
+            this.btnConsulta6.Text = "Consulta6";
+            this.btnConsulta6.UseVisualStyleBackColor = true;
+            // 
+            // btnConsulta7
+            // 
+            this.btnConsulta7.Location = new System.Drawing.Point(454, 27);
+            this.btnConsulta7.Name = "btnConsulta7";
+            this.btnConsulta7.Size = new System.Drawing.Size(197, 23);
+            this.btnConsulta7.TabIndex = 7;
+            this.btnConsulta7.Text = "Consulta7";
+            this.btnConsulta7.UseVisualStyleBackColor = true;
+            // 
+            // btnConsulta8
+            // 
+            this.btnConsulta8.Location = new System.Drawing.Point(454, 74);
+            this.btnConsulta8.Name = "btnConsulta8";
+            this.btnConsulta8.Size = new System.Drawing.Size(197, 23);
+            this.btnConsulta8.TabIndex = 8;
+            this.btnConsulta8.Text = "Consulta8";
+            this.btnConsulta8.UseVisualStyleBackColor = true;
+            // 
+            // btnConsulta9
+            // 
+            this.btnConsulta9.Location = new System.Drawing.Point(454, 123);
+            this.btnConsulta9.Name = "btnConsulta9";
+            this.btnConsulta9.Size = new System.Drawing.Size(197, 23);
+            this.btnConsulta9.TabIndex = 9;
+            this.btnConsulta9.Text = "Consulta9";
+            this.btnConsulta9.UseVisualStyleBackColor = true;
+            // 
+            // btnConsulta10
+            // 
+            this.btnConsulta10.Location = new System.Drawing.Point(657, 74);
+            this.btnConsulta10.Name = "btnConsulta10";
+            this.btnConsulta10.Size = new System.Drawing.Size(197, 23);
+            this.btnConsulta10.TabIndex = 10;
+            this.btnConsulta10.Text = "Consulta10";
+            this.btnConsulta10.UseVisualStyleBackColor = true;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(868, 450);
+            this.Controls.Add(this.btnConsulta10);
+            this.Controls.Add(this.btnConsulta9);
+            this.Controls.Add(this.btnConsulta8);
+            this.Controls.Add(this.btnConsulta7);
+            this.Controls.Add(this.btnConsulta6);
+            this.Controls.Add(this.btnConsulta5);
+            this.Controls.Add(this.btnConsulta4);
+            this.Controls.Add(this.btnConsulta3);
+            this.Controls.Add(this.btnConsulta2);
+            this.Controls.Add(this.btnConsulta1);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Principal";
+            this.Text = "Gestión de productos.";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnConsulta1;
+        private System.Windows.Forms.Button btnConsulta2;
+        private System.Windows.Forms.Button btnConsulta3;
+        private System.Windows.Forms.Button btnConsulta4;
+        private System.Windows.Forms.Button btnConsulta5;
+        private System.Windows.Forms.Button btnConsulta6;
+        private System.Windows.Forms.Button btnConsulta7;
+        private System.Windows.Forms.Button btnConsulta8;
+        private System.Windows.Forms.Button btnConsulta9;
+        private System.Windows.Forms.Button btnConsulta10;
     }
 }
 
